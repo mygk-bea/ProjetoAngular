b3af8907e53893ac924909eeb90b884ed2cead0a
Beatriz Meyagusko <100007869+mygk-bea@users.noreply.github.com>
2023-06-22 13:48:38 +0000

    calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,29 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
+
 export class HomePage {
-  constructor() {}
+  resultado: number = 0;
+  expressao: string = '';
+
+  exibir(tecla: string){
+    this.expressao += tecla;
+    console.log(this.expressao);
+  }
+
+  executar(tecla: string){
+    if(tecla == "C") {
+      this.expressao = '';
+    }
+
+    if(tecla == "+/-") {
+      let ultimoItem = this.expressao.split('')[this.expressao.length-1];
+
+      this.expressao.split('').splice(this.expressao.split('').indexOf(this.expressao.length-1),1,'-' + ultimoItem);
+
+      console.log();
+    }
+  }
 }
