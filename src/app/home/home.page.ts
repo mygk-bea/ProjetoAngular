67bba55c6cc9e1cbccb7b0361808362e8d91e39b
Beatriz Meyagusko <100007869+mygk-bea@users.noreply.github.com>
2023-06-27 01:54:58 +0000

    feature funcionalidade basica

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,21 +13,57 @@ import { IonicModule } from '@ionic/angular';
 export class HomePage {
   resultado: number = 0;
   expressao: string = '';
-
+  
   exibir(tecla: string){
     this.expressao += tecla;
-    console.log(this.expressao);
+    var expressao_array = this.expressao.split('');
+
+    // Vericando se a duplicidade de sinais é real
+    (!Number(tecla) && !Number(expressao_array[expressao_array.length-2])) ?
+    expressao_array.splice(expressao_array.length-2, 2, tecla) : null
+    // Substitui o sinal pela última tecla inserida, caso for verídico
+
+    // atualizando a expressão
+    this.expressao = expressao_array.join('');
+
+    console.log(expressao_array);
+
+    // PORCENTAGEM
+    // DECIMAL
   }
 
   executar(tecla: string){
-    if(tecla == "C") {
+    
+    if(tecla == "clear") {
       this.expressao = '';
     }
 
-    if(tecla == "+/-") {
-      let ultimoItem = this.expressao.split('')[this.expressao.length-1];
+    if(tecla == "backspace") {
+      let expressao_array = this.expressao.split('');
+      expressao_array.pop();
+      this.expressao = expressao_array.join('');
+    }
 
-      console.log();
+    if(tecla == "change") {
+      let expressao_array = this.expressao.split("+");
+      let ultimoNumero = Number(expressao_array[expressao_array.length-1]) * -1;
+
+      expressao_array.splice(expressao_array.length-1, 1, `(${ultimoNumero})`);
+      
+      this.expressao = expressao_array.join('');
+
+      console.log(ultimoNumero)
+      console.log(expressao_array)
+      
+      // atribuir uma variavel como number a : number = expressao_array[expressao_array.lenght -1] * -1
+      // substitui o ultimo item do array
+      // TERMINAR
+    }
+
+    if(tecla == "="){
+      this.resultado = eval(this.expressao);
+      // atribui resultado a expressao
+      // se o valor do proximo item for um numero, substitui o resultado
     }
   }
 }
