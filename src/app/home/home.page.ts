19da987941d508f4935f66e7fd6b367dbc9394aa
Beatriz Meyagusko <100007869+mygk-bea@users.noreply.github.com>
2023-06-29 01:59:20 +0000

    finalizado typescript

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,72 +11,133 @@ import { IonicModule } from '@ionic/angular';
 })
 
 export class HomePage {
-  resultado: number = 0;
-  expressao_array: string[] = [''];
-  indice_array: number = 0;
+  expressao: string = '';
+  expr_Arr: string[] = [''];
+  index_Arr = 0;
+  resultado: string = '';
   verifica_igual: boolean = false;
+  sinais = ['+', '-', '*', '/'];
+
+  criarElemento() {
+    this.index_Arr++;
+    this.expr_Arr[this.index_Arr] = '';
+  }
 
   adicionar(tecla: string){
-    if(!isNaN(Number(tecla)) || tecla == ',') {
-      this.expressao_array[this.indice_array] += tecla;
+    if(tecla == '.' && !Number(this.expr_Arr[this.expr_Arr.length-1])){
+      this.expressao += "0";
+      this.expr_Arr[this.index_Arr] += "0";
+    } else if((tecla == '%' && !Number(this.expr_Arr[this.expr_Arr.length-1]))){
+      tecla = '';
+    } else if(this.expressao == '' && (this.sinais.includes(tecla) || tecla == "%")){
+      tecla = '';
+    }
+
+    // Verificando se a duplicidade de sinais é real
+    if(this.sinais.includes(tecla) && this.sinais.includes(this.expressao.split('')[this.expressao.length-1])) {
+      let array = this.expressao.split('');
+
+      // Substituindo no array e na string o último sinal
+      array.splice(array.length-1, 1, tecla);
+      this.expr_Arr.splice(this.expr_Arr.length-2, 1, tecla);
+      this.expressao = array.join('');
     } else {
-      this.indice_array++;
-      this.expressao_array[this.indice_array] = tecla;
+      if(tecla == "." && this.expr_Arr[this.index_Arr].includes(".")){
+        tecla = '';
+      }
+      // Adicionando a tecla inserida na string
+      if(this.sinais.includes(tecla)){
+        this.expressao += ` ${tecla} `;
+      } else {
+        this.expressao += tecla;
+      }
+      
+      // Caso a tecla seja um número, ou uma vírgula, irá inserir no mesmo index do array
+      if(tecla != "%") {
+        if(Number(tecla) && this.expr_Arr[this.index_Arr] == "/100"){
+          this.expr_Arr[this.index_Arr] += `*${tecla}`;
+        } else if(!isNaN(Number(tecla)) || tecla == '.') {
+          this.expr_Arr[this.index_Arr] += tecla;
+        } else {
+          this.index_Arr++;
+          this.expr_Arr[this.index_Arr] = ` ${tecla} `;
+          this.criarElemento();
+        }
+      }
+
+      console.log(this.expr_Arr)
+      let result = this.expr_Arr.join('');
+      this.resultado = eval(result);
     }
 
-    // Vericando se a duplicidade de sinais é real
-    if(!Number(tecla) && !Number(this.expressao_array[this.expressao_array.length-2])){
-      this.expressao_array.splice(this.expressao_array.length-2, 2, tecla);
-      // Substitui o sinal pela última tecla inserida, caso for verídico
-      this.indice_array--;
-    }
-
-    console.log(this.expressao_array);
-
-    // if(typeof Number(tecla) === "number" && this.verifica_igual){
-    //   this.resultado = Number(tecla);
-    //   this.expressao_array = [tecla];
-    //   this.verifica_igual = false;
-    // }
-
-    // PORCENTAGEM
-    // DECIMAL
+    // Porcentagem
+    if(tecla == '%') {
+      this.index_Arr++;
+      this.expr_Arr[this.index_Arr] = "/100";
+      console.log(this.expr_Arr)
+    } 
   }
 
   executar(tecla: string){
-    
     if(tecla == "clear") {
-      this.expressao_array = [''];
-      this.indice_array = 0;
+      this.expressao = '';
+      this.expr_Arr = [''];
+      this.index_Arr = 0;
+
+      this.resultado = '';
     }
 
     if(tecla == "backspace") {
-      this.expressao_array.pop();
-      this.indice_array--;
-      console.log(this.expressao_array)
+      let array = this.expressao.split('');
+      array.pop();
+      this.expressao = array.join('');
+
+      let apagar = this.expr_Arr[this.expr_Arr.length-1].split('');
+      apagar.pop();
+      this.expr_Arr[this.expr_Arr.length-1] = apagar.join('');
+
+      if(this.expr_Arr[this.expr_Arr.length-1] == '') {
+        this.expr_Arr.pop();
+        this.criarElemento();
+      } else if(this.expr_Arr.length == 0){
+        this.expr_Arr = [''];
+      }
     }
 
     if(tecla == "change") {
-      let ultimoNumero = Number(this.expressao_array[this.indice_array]) * -1;
+      let item = Number(this.expr_Arr[this.index_Arr]) * -1;
+      this.expr_Arr.splice(this.index_Arr, 1, String(item));
 
-      this.expressao_array.splice(this.indice_array, 1, `${ultimoNumero}`);
-      
-      console.log(ultimoNumero)
-      console.log(this.expressao_array)
-      
-      // atribuir uma variavel como number a : number = expressao_array[expressao_array.lenght -1] * -1
-      // substitui o ultimo item do array
-      // TERMINAR
+      let numerosExpr = this.expressao.split(' ');
+      let itemString = `${Number(numerosExpr[numerosExpr.length-1]) * -1}`;
+
+      numerosExpr.splice(numerosExpr.length-1, 1, itemString);
+
+      this.expressao = numerosExpr.join(' ')
     }
 
   }
 
   igual() { 
-    let expressao = this.expressao_array.join('');
-    this.resultado = eval(expressao);
-    this.indice_array = 0;
-    this.expressao_array = [String(this.resultado)];
-    this.indice_array++;
+    if(this.expr_Arr.length > 2 && this.expr_Arr[this.expr_Arr.length-1] == "/100") {
+      let resultado_porcentagem = '';
+      for(let i=0; i<this.expr_Arr.length-3; i++){
+        resultado_porcentagem += this.expr_Arr[i];
+      }
+      this.index_Arr++;
+      this.expr_Arr[this.index_Arr] = `*${eval(resultado_porcentagem)}`;
+      this.criarElemento();
+    }
+
+    let result = this.expr_Arr.join('');
+    this.resultado = String(eval(result));
+    this.index_Arr = 0;
+    
+    this.expressao = this.resultado;
+    this.expr_Arr = [this.resultado];
+    this.criarElemento();
+
+    console.log(this.expr_Arr)
     return this.verifica_igual = true;
   }
 }
